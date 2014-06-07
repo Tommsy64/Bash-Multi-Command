65afd0d2843e95cd7291c8cf86b8caf5ca96eb86
Tommsy64 <thomas.pakh@gmail.com>
2014-06-07 08:53:10 -0400

    First Commit!

---
+++ src/main/java/io/github/tommsy64/bashmulticommand/BashMultiCommand.java
@@ -0,0 +1,15 @@
+package io.github.tommsy64.bashmulticommand;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BashMultiCommand extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		// TODO Insert logic to be performed when the plugin is enabled
+	}
+
+	@Override
+	public void onDisable() {
+		// TODO Insert logic to be performed when the plugin is disabled
+	}
+}
